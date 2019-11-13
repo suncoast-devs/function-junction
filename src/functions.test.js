08d29edb9d4759ddb38f30b499a74feb9f2a43ff
Mark Dewey <mark.t.dewey@gmail.com>
2019-11-13 13:27:54 -0500

    added defined tests and numbered problems

--- src/functions.test.js
+++ src/functions.test.js
@@ -1,5 +1,5 @@
 /**
- * Define a function max() that takes two numbers as arguments
+ * 1) Define a function max() that takes two numbers as arguments
  * and returns the largest of them. Use the if-then-else
  * construct available in Javascript.
  */
@@ -7,35 +7,35 @@
 // ...
 
 /**
- * Define a function maxOfThree() that takes three
+ * 2) Define a function maxOfThree() that takes three
  * numbers as arguments and returns the largest of them.
  */
 
 // ...
 
 /*
- * Define a function sum() that takes two numbers as
+ * 3) Define a function sum() that takes two numbers as
  * arguments and computes the sum of those two numbers.
  */
 
 // ...
 
 /*
- * Define a function sumOfArray that calculates the sum of
+ * 4) Define a function sumOfArray that calculates the sum of
  * all the numbers in an array.
  */
 
 // ...
 
 /**
- * Write a function isVowel() that takes a character (i.e. a string of length 1)
+ * 5) Write a function isVowel() that takes a character (i.e. a string of length 1)
  * and returns true if it is a vowel, false otherwise.
  */
 
 // ...
 
 /**
- * Write a function rovarspraket() that will translate
+ * 6) Write a function rovarspraket() that will translate
  * a text into a "rövarspråket". That is, double every
  * consonant and place an occurrence of "o" in between.
  *
@@ -46,7 +46,7 @@
 // ...
 
 /**
- * Define a function reverse() that computes
+ * 7) Define a function reverse() that computes
  * the reversal of a string. For example,
  * reverse("skoob") should return the
  * string "books".
@@ -55,7 +55,7 @@
 // ...
 
 /**
- * Write a function findLongestWord() that takes an
+ * 8) Write a function findLongestWord() that takes an
  * string returns the first, longest word in the array.
  *
  * i.e. findLongestWord("book dogs") should return "book"
@@ -70,8 +70,17 @@
 /* eslint-disable no-undef */
 
 import test from 'ava'
-
+const ensureDefined = (t, method) => {
+  if (eval(`typeof ${method}`) !== 'function') {
+    t.fail(
+      `\n\n\n\n\n⚡️⚡️⚡️⚡️⚡️ The next step is to define the function ${method} ⚡️⚡️⚡️⚡️⚡️\n\n\n`
+    )
+  } else {
+    t.pass()
+  }
+}
 test('max()', t => {
+  ensureDefined(t, 'max')
   t.is(max(1, 3), 3)
   t.is(max(0, 3), 3)
   t.is(max(10, 3), 10)
@@ -79,6 +88,8 @@ test('max()', t => {
 })
 
 test('maxOfThree()', t => {
+  ensureDefined(t, 'maxOfThree')
+
   t.is(maxOfThree(1, 3, 2), 3)
   t.is(maxOfThree(0, 3, -1), 3)
   t.is(maxOfThree(10, 3, 50), 50)
@@ -86,11 +97,15 @@ test('maxOfThree()', t => {
 })
 
 test('sum()', t => {
+  ensureDefined(t, 'sum')
+
   t.is(sum(8, 11), 19)
   t.is(sum(4, 100), 104)
 })
 
 test('sumOfArray()', t => {
+  ensureDefined(t, 'sumOfArray')
+
   t.is(sumOfArray([1, 2]), 3)
   t.is(sumOfArray([1, 2, 3]), 6)
   t.is(sumOfArray([10, 9, 8]), 27)
@@ -98,6 +113,8 @@ test('sumOfArray()', t => {
 })
 
 test('isVowel()', t => {
+  ensureDefined(t, 'isVowel')
+
   t.is(isVowel('B'), false)
   t.is(isVowel('b'), false)
   t.is(isVowel('t'), false)
@@ -106,6 +123,8 @@ test('isVowel()', t => {
 })
 
 test('rovarspraket()', t => {
+  ensureDefined(t, 'rovarspraket')
+
   t.is(rovarspraket('a'), 'a')
   t.is(rovarspraket('b'), 'bob')
   t.is(rovarspraket('cat'), 'cocatot')
@@ -113,21 +132,29 @@ test('rovarspraket()', t => {
 })
 
 test('reverse()', t => {
+  ensureDefined(t, 'reverse')
+
   t.is(reverse('books'), 'skoob')
   t.is(reverse("we don't want no trouble"), "elbuort on tnaw t'nod ew")
 })
 
 test('findLongestWord()', t => {
+  ensureDefined(t, 'findLongestWord')
+
   t.is(findLongestWord('book dogs'), 'book')
   t.is(findLongestWord('life the universe and everything'), 'everything')
 })
 
 test('max() can handle non numbers', t => {
+  ensureDefined(t, 'max')
+
   t.is(max('aaa', 0), 0)
   t.true(isNaN(max('aaa', 'bbb')))
 })
 
 test('maxOfThree() can handle non numbers', t => {
+  ensureDefined(t, 'maxOfThree')
+
   t.is(maxOfThree('aaa', 0, 1), 1)
   t.true(isNaN(maxOfThree('aaa', 'bbb', 'ccc')))
 })
